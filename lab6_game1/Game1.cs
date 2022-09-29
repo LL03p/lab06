53e38bb46ed8f2cc45df4663f4973fc38e4a5f7a
CAMT <CAMT@DESKTOP-325D3AA>
2022-09-29 14:03:15 +0700

    Add project files.

---
+++ lab6_game1/Game1.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace lab6_game1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
+        Texture2D ballTexture;
+        Texture2D charTexture;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            charTexture = Content.Load<Texture2D>("Char01");
+            ballTexture = Content.Load<Texture2D>("ball");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+
+            base.Draw(gameTime);
+        }
+    }
+}
